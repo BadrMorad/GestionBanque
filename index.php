c4aaf405d0762e6738c83e6e9e895a44b376cf5e
BadrMorad <moradpro989@gmail.com>
2024-11-01 21:10:31 +0100

    php

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php
 
-include 'Titulaire.php'; // Charge la définition de la classe Titulaire depuis le fichier Titulaire.php
+include 'Titulaire.php'; // Inclure le fichier Titulaire.php
+include 'CompteBancaire.php'; // Inclure le fichier CompteBancaire.php
 
-include 'CompteBancaire.php'; // Charge la définition de la classe Compte depuis le fichier CompteBancaire.php
+// Créer une instance de Titulaire
+$titulaire = new Titulaire("Morad", "Badr", "1994-03-09", "brunstatt");
 
-// Créer une instance de la classe Titulaire avec les informations d'un nouvel auteur
-
-$titulaire = new Titulaire("Morad", "Badr", "1994-03-09", "brunstatt"); 
-
-// Ici, on crée un nouvel objet Titulaire en lui passant le prénom, le nom et la date de naissance et la ville
-
+// Créer des instances de CompteBancaire
 $compte1 = new CompteBancaire("Compte Courant", 500, "€", $titulaire);
-
 $compte2 = new CompteBancaire("Livret Jeune", 500, "€", $titulaire);
-
 $compte3 = new CompteBancaire("Livret PEL", 500, "€", $titulaire);
-
 $compte4 = new CompteBancaire("Livret Bloqué", 500, "€", $titulaire);
 
-// echo "$titulaire, " . $titulaire->getAge() . " ans.<br>";
-// var_dump($compte1);
-
-echo $compte1 ;
+// Afficher les comptes
+echo $compte1;
+echo $compte2;
+echo $compte3;
+echo $compte4;
 
 // var_dump($compte1);
 
